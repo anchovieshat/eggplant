9ffe750420fde28c23c57888aa241691bd1a4e46
Colin Davidson <colrdavidson@gmail.com>
2014-11-03 22:33:20 -0500

    Is borked, scenegraph change in progress

--- src/graphics/scenegraph.rs
+++ src/graphics/scenegraph.rs
@@ -1,87 +1,98 @@
-use std::cell::RefCell;
+use std::default::Default;
 use std::rc::Rc;
+use std::cell::RefCell;
 
 trait Render {
     fn render(&mut self);
 }
 
-impl<T: Render> Render for Rc<RefCell<T>> {
-    fn render(&mut self) { self.borrow_mut().render(); }
+struct Node {
+    children: Vec<Box<Node>>,
+    parent: Option<Box<Node>>,
+    object: Option<Rc<RefCell<Box<Render+'static>>>>,
 }
 
-trait Node : Render {
-    fn insert(&mut self, obj: Box<Node + 'static>) { }
-    fn find(&mut self, obj: &Node) -> Option<uint> { None }
-    fn remove(&mut self, idx: uint) { }
+impl Render for Option<Rc<RefCell<Box<Render+'static>>>> {
+    fn render(&mut self) {
+        self.unwrap().borrow().render();
+    }
 }
 
-impl<T: Node> Node for Rc<RefCell<T>> {
-    fn insert(&mut self, obj: Box<Node + 'static>) { self.borrow_mut().insert(obj) }
-    fn find(&mut self, obj: &Node) -> Option<uint> { self.borrow_mut().find(obj) }
-    fn remove(&mut self, idx: uint) { self.borrow_mut().remove(idx) }
-}
-
-struct SceneGraph {
-    children: Vec<Box<Node + 'static>>
-}
-
-impl SceneGraph {
-    fn new() -> SceneGraph {
-        SceneGraph {
+impl Node {
+    fn new(object: Rc<RefCell<Box<Render+'static>>>, parent: Option<Box<Node>>) -> Node {
+        Node {
+            parent: parent,
             children: Vec::new(),
+            object: Some(object),
         }
     }
-}
 
-impl Node for SceneGraph {
-    fn insert(&mut self, obj: Box<Node + 'static>) {
-        self.children.push(obj);
+    fn insert(&mut self, child: Box<Node>) {
+        self.children.push(child);
     }
 
-    fn find(&mut self, obj: &Node) -> Option<uint> {
-        None
-    }
-
-    fn remove(&mut self, idx: uint) {
-    }
-
-}
-
-impl Render for SceneGraph {
     fn render(&mut self) {
+        self.object.render();
         for child in self.children.iter_mut() {
             child.render();
         }
     }
 }
 
+impl Default for Node {
+    fn default() -> Node {
+        Node {
+            parent: None,
+            children: Vec::new(),
+            object: None
+        }
+    }
+}
+
+struct SceneGraph {
+    parent: Box<Node>,
+}
+
+impl SceneGraph {
+    fn new(parent: Box<Node>) -> SceneGraph {
+        SceneGraph {
+            parent: parent,
+        }
+    }
+
+    fn render(&mut self) {
+        self.parent.render();
+    }
+}
 
 #[cfg(test)]
 mod test {
     use super::{SceneGraph, Render, Node};
+    use std::default::Default;
     use std::fmt::Show;
-    use std::cell::RefCell;
+    use std::any::Any;
+    use std::boxed::BoxAny;
     use std::rc::Rc;
+    use std::cell::RefCell;
 
-    struct TestNode {
+    struct TestObj {
         rendered: bool,
     }
 
-    impl Render for TestNode {
+    impl Render for TestObj {
         fn render(&mut self) { self.rendered = true; }
     }
 
-    impl Node for TestNode { }
-
     #[test]
     fn test_traversal() {
-        let mut graph = SceneGraph::new();
-        let mut test_node = Rc::new(RefCell::new(TestNode { rendered: false }));
+        let mut graph = SceneGraph::new(Default::default());
+        let testobj = box TestObj { rendered: false };
+        let mut obj = Rc::new(RefCell::new(testobj));
 
-        graph.insert(box test_node.clone());
+        graph.parent.insert(box Node::new(obj.clone(), None));
 
         graph.render();
 
-        assert_eq!(test_node.borrow_mut().rendered, true);
+        assert_eq!(obj.borrow().rendered, true);
     }
 }
